e6204fe986a9ca82b082a80ce6d0b8842ca7039f
nauravalda <16521058@mahasiswa.itb.ac.id>
2022-10-30 19:43:13 +0700

    mesin_kata = mesin_game

--- src/ADT/mesin_kata.h
+++ src/ADT/mesin_kata.h
@@ -1,8 +1,9 @@
 /* File: mesinkata.h */
 /* Definisi Mesin Kata: Model Akuisisi Versi I */
+// 1 kata pada ADT ini adalah 1 game
 
 #ifndef __MESINKATA_H__
-#define __MESINKATA_H__
+#define __MESINGAME_H__
 
 #include "boolean.h"
 #include "mesin_kar.h"
