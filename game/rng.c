1f67e9920b294cd558557685b52a82231b3bf261
Ahmad Rizki <amrizjr@gmail.com>
2022-10-31 17:40:34 +0700

    ngeprint config.txt

--- game/rng.c
+++ game/rng.c
@@ -2,23 +2,6 @@
 #include <stdio.h>
 #include <time.h>
 #include <stdlib.h>
-int main(){
-    srand(time(NULL));
-    printf("RNG Telah dimulai.\n");
-    printf("Uji keberuntungan Anda dengan menebak X\n");
-    int x=0;//inisiasi awal
-    printf("Tebakan: ");
-    scanf("%d",&x);
-    int random=(rand()%100)+1;//batasan X 100
-    tebakan(x,random);
-    while (random!=x){
-        printf("Tebakan: ");
-        scanf("%d",&x);
-        tebakan(x,random);
-    }    
-    return 0;
-}
-
 
 void tebakan (int x, int random){
     if (x<random){
@@ -31,4 +14,20 @@ void tebakan (int x, int random){
         printf("Ya, X adalah %d",x);
         }  
 }
-//test
+
+int main(){
+    srand(time(NULL));
+    printf("RNG Telah dimulai.\n");
+    printf("Uji keberuntungan Anda dengan menebak X (0 <= X <= 100)\n");
+    int x=0;//inisiasi awal
+    printf("Tebakan: ");
+    scanf("%d",&x);
+    int random=(rand()%100)+1;//batasan X 100
+    tebakan(x,random);
+    while (random!=x){
+        printf("Tebakan: ");
+        scanf("%d",&x);
+        tebakan(x,random);
+    }    
+    return 0;
+}
