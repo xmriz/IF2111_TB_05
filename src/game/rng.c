8b0cda205f97a0ccdf935a85431aa8ca7f034979
nauravalda <16521058@mahasiswa.itb.ac.id>
2022-11-01 13:22:01 +0700

    mindahin dir game

---
+++ src/game/rng.c
@@ -0,0 +1,33 @@
+/*Game RNG*/
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+void tebakan (int x, int random){
+    if (x<random){
+        printf("Lebih besar\n");
+        }
+    else if (x>random){
+        printf("Lebih kecil\n");
+    }
+    else{
+        printf("Ya, X adalah %d",x);
+        }  
+}
+
+int main(){
+    srand(time(NULL));
+    printf("RNG Telah dimulai.\n");
+    printf("Uji keberuntungan Anda dengan menebak X (0 <= X <= 100)\n");
+    int x=0;//inisiasi awal
+    printf("Tebakan: ");
+    scanf("%d",&x);
+    int random=(rand()%100)+1;//batasan X 100
+    tebakan(x,random);
+    while (random!=x){
+        printf("Tebakan: ");
+        scanf("%d",&x);
+        tebakan(x,random);
+    }    
+    return 0;
+}
