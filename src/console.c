bcc52cf3603aaa0d4d5b42aa79836c80d7e8b8e1
Pramaditya Fajri Migfar <89334139+fajrimigfar@users.noreply.github.com>
2022-11-03 15:54:09 +0700

    update scan buat create game command

--- src/console.c
+++ src/console.c
@@ -125,8 +125,8 @@ void save(char* filename, TabGame listgame, int n_game, QueueGame history){
 
 void createGame(int n_game, TabGame listgame) {
     printf("Masukkan nama game yang akan ditambahkan: ");
-    STARTKALIMAT();
-    listgame.TG[n_game] = CKalimat;
+    STARTINPUT();
+    listgame.TG[n_game] = CInput;
     n_game++;
     (listgame.Neff)++;
     printf("\nGame berhasil ditambahkan\n");
