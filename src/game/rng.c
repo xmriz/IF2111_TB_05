399af4971b72876b6fed635d9fc0ed477db05317
nadinealiya <16521289@std.stei.itb.ac.id>
2022-11-09 18:53:53 +0700

    Update rng.c
    nambahin limit tebakan sama skor rng

--- src/game/rng.c
+++ src/game/rng.c
@@ -3,6 +3,8 @@
 #include <time.h>
 #include <stdlib.h>
 #include "../console.h"
+//limit percobaan 10 kali
+//skor awal 1000, tiap percobaan yang salah skor akan berkurang 100
 
 void tebakan (int x, int random){
     if (x<random){
@@ -20,14 +22,24 @@ void mainRNG(){
     srand(time(NULL));
     printf("RNG Telah dimulai.\n");
     printf("Uji keberuntungan Anda dengan menebak X (0 <= X <= 100)\n");
+    printf("Anda dapat menebak maksimal 10 kali! \n");
     int x=0;//inisiasi awal
+    int count=1;
     printf("Tebakan: ");
     x = scanint();
     int random=(rand()%100)+1;//batasan X 100
     tebakan(x,random);
-    while (random!=x){
+    while ((random!=x)&&(count<10)){
         printf("Tebakan: ");
         x = scanint();
         tebakan(x,random);
-    }    
+        count+=1;
+    }   
+    if (count==10){
+        printf("Maaf, anda sudah mencapai limit tebakan!\n");
+        printf("Skor anda = 0 \n");
+    }
+    else{
+        printf("Skor anda = %d ", 1000-(count*100));
+    }
 }
