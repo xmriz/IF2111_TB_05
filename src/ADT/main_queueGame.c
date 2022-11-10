c988df75aa49edf9a98e9bff7827ae58b046e95e
Pramaditya Fajri Migfar <89334139+fajrimigfar@users.noreply.github.com>
2022-11-11 01:58:32 +0700

    add driver queueGame

---
+++ src/ADT/main_queueGame.c
@@ -0,0 +1,24 @@
+/* File : main_queueGame.c */
+/* Program MainQueueGame */
+/* Driver ADT QueueGame */
+#include <stdio.h>
+#include "queueGame.h"
+
+int main()
+{
+    /* KAMUS */
+    QueueGame q;
+    Kalimat val;
+    /* ALGORITMA*/
+    CreateQueueGame(&q);
+    printf("Terbuat queueGame yang kosong (1 = true, 2 = false): %d\n", isEmptyGame(q));
+    printf("Terbuat queueGame yang full (1 = true, 2 = false): %d\n", isFullGame(q));
+    printf("Kalimat yang ingin dimasukkan ke queue: ");
+    STARTKALIMAT();
+    enqueueGame(&q, CKalimat);
+    printf("Banyaknya elemen queue: %d\n", lengthGame(q) + 1);
+    dequeueGame(&q, &val);
+    printf("Banyaknya elemen queue setelah dequeue: %d\n", lengthGame(q));
+    printf("Isi queue: ");
+    displayQueueGame(q);
+}
