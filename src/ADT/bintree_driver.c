ac3f814e7cca417b4b5d7ee3c423a957ca701940
Pramaditya Fajri Migfar <89334139+fajrimigfar@users.noreply.github.com>
2022-12-02 17:31:21 +0700

    Update bintree_driver.c

--- src/ADT/bintree_driver.c
+++ src/ADT/bintree_driver.c
@@ -1,63 +1,26 @@
-#include "bintree.h"
+/* File : bintree_driver.c */
+/* Program MainBintree */
+/* Driver ADT Bintree */
 #include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
+#include "bintree.h"
 
-void delay(int number_of_seconds)
+int main()
 {
-    // Converting time into milli_seconds
-    int milli_seconds = 1000 * number_of_seconds;
- 
-    // Storing start time
-    clock_t start_time = clock();
- 
-    // looping till required time is not achieved
-    while (clock() < start_time + milli_seconds)
-        ;
-}
-
-BinTree BuildBalanceTree(int n)
-/* Menghasilkan sebuah balanced tree dengan n node, nilai setiap node dibaca */
-/* Jika n == 0, kembalikan Nil.
-   Mula-mula, baca nilai dari root dari stdin, lalu bangun pohon biner di kiri
-   (dengan membaca dari stdin) lalu di tree kanan (dengan membaca dari stdin).
-   misal dari stdin: 1, 2, 3, 4, 5, 6, 7, hasilnya:
-       1
-     2   5
-    3 4 6 7
-  */
-{
-  AddressT P;
-  ElTypeT X;
-  BinTree R, L;
-  int nL, nR;
-  if (n == 0)
-  {
-    return NULL;
-  }
-  else
-  {
-    delay(1);
-    srand(time(NULL));
-    X.isTreasure=0;
-    X.random=rand()%100;
-    P = newTreeNode(X);
-    if (P != NULL)
-    {
-      ROOT(P) = X;
-      nL = n / 2;
-      nR = n - nL - 1;
-      L = BuildBalanceTree(nL);
-      R = BuildBalanceTree(nR);
-      LEFT(P) = L;
-      RIGHT(P) = R;
-    }
-    return P;
-  }
-}
-
-int main(){
-    BinTree T=BuildBalanceTree(7);
-    printPostorder(T);
-
+    BinTree a, b, c;
+    ElTypeT info1, info2;
+    a = NULL;
+    printf("Terbuat tree a yang kosong (1 = true, 0 = false): %d\n", isTreeEmpty(a));
+    info1.random = 10;
+    info1.isTreasure = 20;
+    b = NewTree(info1, NULL, NULL);
+    printf("Terbuat tree b 1 element (1 = true, 0 = false): %d\n", isOneElmt(b));
+    info2.random = 5;
+    info2.isTreasure = 7;
+    CreateTree(info2, newTreeNode(info1), NULL, &c);
+    printf("Apakah tree c adalah uner left (1 = true, 0 = false): %d\n", isUnerLeft(c));
+    printf("Apakah tree c adalah uner right (1 = true, 0 = false): %d\n", isUnerRight(c));
+    printf("Apakah tree c adalah binary (1 = true, 0 = false): %d\n", isBinary(c));
+    printf("Hasil print tree c adalah\n");
+    printPostorder(c);
+    deallocTreeNode(a);
 }
